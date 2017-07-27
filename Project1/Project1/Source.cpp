01fdaeeaa2fc0e6a36d0b1a2414ef46a8421eef9
victoriyaap <viktory.p@list.ru>
2017-07-27 09:36:14 +0300

    //

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <dlib/algs.h>
+using namespace std;
+using namespace dlib;
+
+
+int main()
+{
+	unsigned long num;
+	cout << "enter num \n" ;
+	cin >> num;
+	cout << square_root(num);
+	system ("pause");
+	return 0;
+}
+
---
+++ Project1/scr/Source.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <dlib>
+using namespace std;
+
+
+int main()
+{
+	cout << "hello World" ;
+	system ("pause");
+	return 0;
+}
+
